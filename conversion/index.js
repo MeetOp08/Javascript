80c1fe3b31bf4830465edb4b70a7b084ea98f0e0
MeetOp08 <patelmeet52271@gmail.com>
2026-05-24 14:43:19 +0530

    datatypes and conversion Operations

---
+++ conversion/index.js
@@ -0,0 +1,26 @@
+// let score = "33";
+// console.log(typeof(score));//string
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));//number
+
+// //String
+// let score = 33;
+// console.log(typeof(score));//number
+// let stringValue = String(score);
+// console.log(typeof(stringValue));//string
+
+// // Boolean
+// let istrue ="" //false=>"",true=>"meet"
+// istrue = 1;//number
+// let booleanValue = Boolean(istrue);//boolean,true=>1,false=>0
+
+// //NaN(Not a Number)
+// let score  = "33ab";
+// let valueInNumber =  Number(score);
+// console.log(typeof(valueInNumber)); //number
+// console.log(valueInNumber);//NaN
+
+// //Null
+// let score = null;
+// console.log(typeof(score));//object
+// console.log(score);//0
---
+++ datatype/index.js
@@ -0,0 +1,14 @@
+/*DataTypes:
+        number=>2 to power 53,
+        Bitint,
+        string =>" ",
+        boolean =>true/false,
+        null =>standalone value,
+        undefined => value not assign
+        symbol => unique 
+        object => 
+
+            console.log(typeOf(null));//object
+            console.log(typeOf(undefined));//undefined
+*/
+    
