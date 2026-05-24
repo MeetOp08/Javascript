51a3e923d808093835f97bf73bf5925f1d6eec9b
MeetOp08 <patelmeet52271@gmail.com>
2026-05-24 17:58:23 +0530

    premitive and non-premitive datatype

--- datatype/index.js
+++ datatype/index.js
@@ -1,4 +1,6 @@
 /*DataTypes:
+
+    1.premite datatype
         number=>2 to power 53,
         Bitint,
         string =>" ",
@@ -6,7 +8,10 @@
         null =>standalone value,
         undefined => value not assign
         symbol => unique 
-        object => 
+    2.non-premitive datatype
+        Object => 
+        Array =>
+        Functions =>
 
             console.log(typeOf(null));//object
             console.log(typeOf(undefined));//undefined
