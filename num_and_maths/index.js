7840e50dfd4a75595830d747e7186c4ec10ea385
MeetOp08 <patelmeet52271@gmail.com>
2026-05-25 12:03:39 +0530

    num and maths

---
+++ num_and_maths/index.js
@@ -0,0 +1,48 @@
+//Number
+
+// let score = 200;
+// console.log(score);
+// console.log(typeof score);
+
+// let balance = new Number(100);
+// console.log(balance);
+// console.log(typeof balance);
+
+// let toStringNumber = balance.toString()//convert number to String
+// console.log(toStringNumber);
+//console.log(toStringNumber.length);
+// let toFixedNumeber = balance.toFixed(2);//for the decimal(means howmany number show after .)
+// console.log(toFixedNumeber)//100.00
+
+// let otherNumber = 123.79846
+// let precisionNumber = otherNumber.toPrecision(3); //used for precision
+// console.log(precisionNumber);
+
+// let newNumber = 10000000;
+// console.log(newNumber.toLocaleString());
+
+//Maths
+
+// console.log(Math)
+// console.log(Math.abs(-4));//4 convert -ve values to +ve values.con not convert +ve to -ve.
+// console.log(Math.floor(4.3));//4 gives in integer value not in decimal like 4.9 => 4.
+// console.log(Math.max(3,9,8,2,4));//give maximum value
+// console.log(Math.random());//gives ramdom value bteween 0 and 1;
+// console.log((Math.random()*10 )+ 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// console.log(Date());
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleString('default',{weekday:"long"}));
+// console.log(myDate.toTimeString());
+
+// console.log("TimeStamp:",Date.now());
+// console.log(myDate.getTime());
+
+// let newDate = new Date();
+// console.log(newDate.getFullYear())
