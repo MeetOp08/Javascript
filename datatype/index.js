ecb51041c2df437f842f52c100737fe994dc2cb7
MeetOp08 <patelmeet52271@gmail.com>
2026-05-25 11:15:29 +0530

    string

--- datatype/index.js
+++ datatype/index.js
@@ -1,6 +1,6 @@
 /*DataTypes:
 
-    1.premite datatype
+    1.primite datatype(Stake memory used)(stack store copy of the value)
         number=>2 to power 53,
         Bitint,
         string =>" ",
@@ -8,7 +8,7 @@
         null =>standalone value,
         undefined => value not assign
         symbol => unique 
-    2.non-premitive datatype
+    2.non-primitive datatype(Heap memory used)(Heap stored original value)
         Object => 
         Array =>
         Functions =>
---
+++ string/index.js
@@ -0,0 +1,44 @@
+const fullName = new String('Meet patel');
+console.log(fullName);
+
+let anotherName = "jainil Patel";
+
+// console.log(anotherName.charAt('3')); // for find value at specific index
+// console.log(anotherName.indexOf('n'));// for find index of specific value
+
+// let subStringOfName = anotherName.substring(2,8); //In subString we can not use -ve index like .subString(-3,5);
+// console.log(subStringOfName);
+
+// let sliceOfName = anotherName.slice(4,8); //In Sclice we can start with -ve index also like.slic(-8,4);
+// console.log(sliceOfName);
+
+// anotherName = " Jay Patel "
+
+// let trimName = anotherName.trim();//remove space from start and end both side.
+// let trimStartName = anotherName.trimStart();//remove space from start
+// let trimEndName = anotherName.trimEnd();//remove space from end
+
+// console.log(trimName);
+// console.log(trimStartName);
+// console.log(trimEndName);
+
+// let url = "https://www.youtube.com/watch?v=fozwNnFunlo&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=11";
+// let replaceUrl = url.replace('PL','hi');//('what replace','replace value');
+// console.log(replaceUrl);
+
+// let IncludeName=anotherName.includes("jai"); //for check value exist or not
+// console.log(IncludeName);
+
+// let splitName = anotherName.split('i');//split string and return ans in array.
+// console.log(splitName);
+    
+// let joinName = splitName.join("");//it join split array and return ans in string
+// console.log(joinName)
+    
+// let reverseName = anotherName.split(' ').reverse().join(' ');//reverseString
+// console.log(reverseName);//Patel jainil
+// console.log(reverseName.split("").reverse().join(""));/liniaj letaP
+
+// let concatName = fullName.concat(anotherName); //mearge 2 String
+// console.log(concatName);
+
