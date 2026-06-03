abb8c1e581b0f9a61f1f965b4235c6f237549d55
MeetOp08 <patelmeet52271@gmail.com>
2026-06-03 13:14:10 +0530

    loops

--- loops/index.js
+++ loops/index.js
@@ -20,10 +20,11 @@
 //Array/strings  base loop
 // let heros = ["ironman","hulk","hawkeye","loki"];
 // let greeting = "Welcome, Meet Patel"
+
 // 1.for of(print value)
 
 // for(let i of heros){
-//     console.log(i);
+//     console.log(i);//return value in string
 // }
 
 // for(let i of greeting){
@@ -48,4 +49,62 @@
 //     console.log(key);//this will give only key or value base on what you print
 // }
 
-//for object [key,value] this method will not itterable
+// //error:for object [key,value] this method will not itterable
+
+//2.for in (because of above error for in used for object)
+
+// const greet={
+//     name:"Meet",
+//     age:21,
+//     gender:"Male"
+// }
+
+// let heros = ["ironman","hulk","hawkeye","loki"];
+// for(let i in greet){
+//     console.log(i);//this will give keys only
+// }
+
+// for (let key in greet){
+//     console.log(key,":",greet[key]);//this will print key and value
+// }
+
+// for (let i in heros){
+//     console.log(i);//this will give index
+// }
+
+// for (let i in heros){
+//     console.log(heros[i]);//print value in string
+// }
+
+// +++ Note:differece between for of and for in is ....
+// for of return value 
+// for in return index
+// for in is not working for map bcz map is not itterable.
+
+//3. forEach
+
+// const coding = ["python","java","javascript","ruby"];
+
+// coding.forEach((value)=>{
+//     console.log(value + "hi");//return values in string
+// });
+
+// // Object in array [{}]
+// const myCoding = [
+//     {
+//         languageName : "java",
+//         languageFileName : "java"
+//     },
+//     {
+//         languageName : "python",
+//         languageFileName : "py"
+//     },
+//     {
+//         languageName : "javaScript",
+//         languageFileName : "js"
+//     }
+// ];
+
+// myCoding.forEach((item)=>{
+//     console.log(item.languageName);
+// })
