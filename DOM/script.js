49cfe03b40f6ef0b62e16db47535498b62c9557e
MeetOp08 <patelmeet52271@gmail.com>
2026-06-05 12:14:17 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,36 @@
+// text=document.getElementById("text").getAttribute('id')
+// text=document.getElementById("text").getAttribute('class')
+//document.getElementById("heading").setAttribute('class','bihhead')
+
+// title = document.getElementById("text")
+// title.style.backgroundColor="green";
+// title.style.padding=10px;
+
+// title.innerHTML
+//title.textContent
+//title.innertext
+
+// h1 = document.querySelector("h1")//take first h1
+// h1 = document.querySelectorAll("h1")//take all query selector
+
+// myul  = document.querySelector("ul");
+// myul.querySelector("li");
+
+// myli = document.querySelectorAll("li"); // [li,li,li] give nodelist
+
+// myli.style.backgroundColor = "red"// wrong 
+// myli.forEach((item)=>{
+//     item.style.backgroundColor="red";//true
+// })
+
+// let listitem = document.getElementsByClassName("list-item");//undefined
+// listitem//HTMLCollection(3) [li.list-item, li.list-item, li.list-item]
+
+// listitem.forEach((item)=>{
+//     item.style.color="red"
+// })//error
+
+//convert into array then 
+// Array.from(listitem).forEach((item)=>{
+//     item.style.color="red"
+// })
